7e8a33256a0e7995b9675dbab821368d6dab68d4
MhmdRyhn <rayhan.cse12@gmail.com>
2020-05-19 22:49:16 +0600

    Fix CORS enable issue

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -37,7 +37,7 @@ resource "aws_api_gateway_method_response" "root_options_method_response_200" {
   http_method = aws_api_gateway_method.root_options.http_method
   status_code = local.status_code.success
   response_models = {
-    "Application/json" = "Empty"
+    "application/json" = "Empty"
   }
   response_parameters = local.method_response_parameters
 
@@ -79,7 +79,7 @@ resource "aws_api_gateway_integration" "root_get_integration" {
 }
 
 # / GET -> Method Response
-resource "aws_api_gateway_method_response" "root_get_response_200" {
+resource "aws_api_gateway_method_response" "root_get_method_response_200" {
   rest_api_id = aws_api_gateway_rest_api.dummy.id
   resource_id = aws_api_gateway_rest_api.dummy.root_resource_id
   http_method = aws_api_gateway_method.root_get.http_method
@@ -87,6 +87,30 @@ resource "aws_api_gateway_method_response" "root_get_response_200" {
   response_models = {
     "application/json" = "Empty"
   }
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = true
+    "method.response.header.Access-Control-Allow-Headers" = true
+  }
+  depends_on = [
+    aws_api_gateway_method.root_get
+  ]
+}
+
+# / GET -> Integration Response
+resource "aws_api_gateway_integration_response" "root_get_integration_response_200" {
+  rest_api_id = aws_api_gateway_rest_api.dummy.id
+  resource_id = aws_api_gateway_rest_api.dummy.root_resource_id
+  http_method = aws_api_gateway_method_response.root_get_method_response_200.http_method
+  status_code = aws_api_gateway_method_response.root_get_method_response_200.status_code
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = var.allowed_origins
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Requested-With,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
+  }
+
+  depends_on = [
+    aws_api_gateway_integration.root_get_integration,
+    aws_api_gateway_method_response.root_get_method_response_200
+  ]
 }
 
 # / -> Deploy API
