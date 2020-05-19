9a87678826699ac932dbbb83ae00d9f9955e877f
MhmdRyhn <rayhan.cse12@gmail.com>
2020-05-19 17:16:53 +0600

    Adding OPTIONS a.k.a CORS configuration

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -8,15 +8,65 @@ resource "aws_api_gateway_rest_api" "dummy" {
   description = var.api_gateway_description
 }
 
-# GET -> Request Method
+# / OPTIONS -> Method Request
+resource "aws_api_gateway_method" "root_options" {
+  rest_api_id = aws_api_gateway_rest_api.dummy.id
+  resource_id = aws_api_gateway_rest_api.dummy.root_resource_id
+  http_method = local.method.options
+  authorization = "None"
+}
+
+# / OPTIONS -> Integration Request
+resource "aws_api_gateway_integration" "root_option_integration" {
+  rest_api_id = aws_api_gateway_rest_api.dummy.id
+  resource_id = aws_api_gateway_rest_api.dummy.root_resource_id
+  http_method = aws_api_gateway_method.root_options.http_method
+
+  integration_http_method = local.lambda_proxy_integration_method
+  type = local.mock_integration_type
+  depends_on = [aws_api_gateway_method.root_options]
+}
+
+# / OPTIONS -> Method Response
+resource "aws_api_gateway_method_response" "root_option_response_200" {
+  rest_api_id = aws_api_gateway_rest_api.dummy.id
+  resource_id = aws_api_gateway_rest_api.dummy.root_resource_id
+  http_method = aws_api_gateway_method.root_options.http_method
+  status_code = local.status_code.success
+  response_models = {
+    "Application/json" = "Empty"
+  }
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = true
+    "method.response.header.Access-Control-Allow-Methods" = true
+    "method.response.header.Access-Control-Allow-Headers" = true
+  }
+  depends_on = [aws_api_gateway_integration.root_option_integration]
+}
+
+# / OPTIONS -> Integration Response
+resource "aws_api_gateway_integration_response" "root_option_integration_renponse_200" {
+  rest_api_id = aws_api_gateway_rest_api.dummy.id
+  resource_id = aws_api_gateway_rest_api.dummy.root_resource_id
+  http_method = aws_api_gateway_method_response.root_option_response_200.http_method
+  status_code = aws_api_gateway_method_response.root_option_response_200.status_code
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = "'*'"
+    "method.response.header.Access-Control-Allow-Methods" = "'OPTIONS, GET'"
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Requested-With,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
+  }
+  depends_on = [aws_api_gateway_integration.root_option_integration]
+}
+
+# / GET -> Method Request
 resource "aws_api_gateway_method" "root_get" {
   rest_api_id = aws_api_gateway_rest_api.dummy.id
   resource_id = aws_api_gateway_rest_api.dummy.root_resource_id
-  http_method = "GET"
+  http_method = local.method.get
   authorization = "NONE"
 }
 
-# GET -> Integration Request
+# / GET -> Integration Request
 resource "aws_api_gateway_integration" "root_get_integration" {
   rest_api_id = aws_api_gateway_rest_api.dummy.id
   resource_id = aws_api_gateway_method.root_get.resource_id
@@ -27,12 +77,12 @@ resource "aws_api_gateway_integration" "root_get_integration" {
   uri = aws_lambda_function.dummy_ground.invoke_arn
 }
 
-# GET -> Method Response
+# / GET -> Method Response
 resource "aws_api_gateway_method_response" "root_get_response_200" {
   rest_api_id = aws_api_gateway_rest_api.dummy.id
   resource_id = aws_api_gateway_rest_api.dummy.root_resource_id
   http_method = aws_api_gateway_method.root_get.http_method
-  status_code = "200"
+  status_code = local.status_code.success
   response_models = {
     "application/json" = "Empty"
   }
@@ -43,39 +93,14 @@ resource "aws_api_gateway_deployment" "root_deploy_dev" {
   rest_api_id = aws_api_gateway_rest_api.dummy.id
   stage_name = var.api_stage
   depends_on = [
+    aws_api_gateway_integration.root_option_integration,
     aws_api_gateway_integration.root_get_integration,
-    aws_api_gateway_method_response.root_get_response_200
   ]
   variables = {
     deployed_at = timestamp()
   }
 }
 
-/*
-resource "aws_api_gateway_integration_response" "root_integration_response" {
-  rest_api_id = aws_api_gateway_rest_api.dummy.id
-  resource_id = aws_api_gateway_rest_api.dummy.root_resource_id
-  http_method = aws_api_gateway_method.root_get.http_method
-  status_code = aws_api_gateway_method_response.root_get_response_200.status_code
-  response_templates = {
-    "application/json" = ""
-  }
-  depends_on = [
-    aws_api_gateway_integration.root_get_integration
-  ]
-
-  # Transforms the backend JSON response to XML
-  response_templates = {
-    "application/xml" = <<EOF
-#set($inputRoot = $input.path('$'))
-<?xml version="1.0" encoding="UTF-8"?>
-<message>
-    $inputRoot.body
-</message>
-EOF
-  }
-}
-*/
 
 
 # ------------------------ Resource: /{username} -----------------------------
--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,4 +1,14 @@
 locals {
   lambda_proxy_integration_method = "POST"
   lambda_proxy_integration_type = "AWS_PROXY"
+  mock_integration_type = "MOCK"
+  status_code = {
+    success = "200"
+  }
+  method = {
+    options = "OPTIONS"
+    get = "GET"
+    post = "POST"
+  }
 }
+
